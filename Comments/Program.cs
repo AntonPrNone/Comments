f897512968db3b7324a78cf12993ef1efce87daf
Anton <yznikfortyni@gmail.com>
2022-10-20 09:22:24 +0300

    Добавьте файлы проекта.

---
+++ Comments/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Comments
+{
+    internal class Program
+    {
+        internal const int AdditionaCharacters = 13;
+        internal const int MaxLen = 125;
+
+        static void Main(string[] args)
+        {
+            while (true)
+            { 
+                Com(Console.ReadLine());
+                Console.WriteLine();
+            }
+            
+        }
+
+        static void Com(string text)
+        {
+            int Len = text.Length;
+            int numberLines = MaxLen - Len - AdditionaCharacters;
+            for (int i = 0; i < numberLines / 2; i++)
+            { 
+                Console.Write('-');
+            }
+        }
+    }
+}
